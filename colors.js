d8a36008535f94aee39e55a5b725620fc0846d33
James Jung <142671712+Topology0427@users.noreply.github.com>
2023-11-03 19:59:38 +0900

    Add files via upload

---
+++ colors.js
@@ -0,0 +1,33 @@
+var Links = {
+    setColor:function (color){
+        var alist = document.querySelectorAll('a');
+        var i = 0;
+        while(i < alist.length){
+            alist[i].style.color = color;
+            i = i +1;      
+        }
+    }
+}
+var Body = {
+    setColor:function (color){
+        document.querySelector('body').style.color = color;
+    },
+    setBackgroundColor:function (color){
+        document.querySelector('body').style.backgroundColor = color;           
+    }
+}
+function nightDayHandler(self){
+    var target = document.querySelector('body'); 
+    if(self.value === 'night'){
+        Body.setBackgroundColor('black');
+        Body.setColor('LightGoldenRodYellow');
+        self.value = 'day';
+        Links.setColor('yellow');      
+    } else{
+        Body.setBackgroundColor('white');
+        Body.setColor('Salmon');
+        self.value = 'night';
+        
+        Links.setColor('coral');
+    }
+}
